4cb8cacc21954a05d14ba52e7af38952a20fcb67
jebarcha <jebarcha@hotmail.com>
2024-07-18 15:45:45 -0600

    added a FileManager Struct

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
-import "price-calc/prices"
+import (
+	"fmt"
+	"price-calc/filemanager"
+	"price-calc/prices"
+)
 
 func main() {
 	//prices := []float64{10, 20, 30}
 	taxRates := []float64{0, 0.7, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
-		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
 		priceJob.Process()
 	}
 }
